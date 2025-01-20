21d42743391854d24f2fd7da17d24df02827bb7e
Fernando Gomez <zfngomez@gmail.com>
2025-01-19 21:56:48 -0300

    Fix environment variables

--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,3 @@
 const processEnv = process.env;
 
-export const { API_URL = 'http://localhost:3000/api' } = processEnv;
+export const { EXPO_PUBLIC_API_URL: API_URL = 'http://localhost:3000/api' } = processEnv;
